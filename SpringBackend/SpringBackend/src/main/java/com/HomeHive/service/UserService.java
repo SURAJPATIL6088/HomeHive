7e27cffae9e6010ff4b33c13acf5e64063b54894
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2025-07-26 22:57:59 +0530

    Minor Changes with Custom User Error (ENUM) and best practices

--- SpringBackend/SpringBackend/src/main/java/com/HomeHive/service/UserService.java
+++ SpringBackend/SpringBackend/src/main/java/com/HomeHive/service/UserService.java
@@ -1,9 +1,9 @@
 package com.HomeHive.service;
 
 import com.HomeHive.dto.UserResponseDTO;
-import com.HomeHive.dto.userSignInDTO;
-import com.HomeHive.dto.userSignUpDTO;
+import com.HomeHive.dto.UserSignInDTO;
+import com.HomeHive.dto.UserSignUpDTO;
 
 public interface UserService {
-	UserResponseDTO registerNewResidant(userSignUpDTO dto);
+	UserResponseDTO registerNewResidant(UserSignUpDTO dto);
 }
