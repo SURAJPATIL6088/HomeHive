03e69977d6ff7e93c00eafc42f5052317c94dd08
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2025-08-04 01:00:32 +0530

    Facilities Function

--- SpringBackend/SpringBackend/src/main/java/com/HomeHive/dao/UserDao.java
+++ SpringBackend/SpringBackend/src/main/java/com/HomeHive/dao/UserDao.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.HomeHive.entities.User;
 
+@Repository
 public interface UserDao extends JpaRepository<User, Long>{
 	// get details by email
 	Optional<User> findByEmail(String email);
